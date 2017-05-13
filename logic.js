e8759f2cca25156e96c826a485bfb89571bb8367
Paul Richard <Paul Richard>
2017-05-13 13:39:44 -0700

    matched search button div

--- logic.js
+++ logic.js
@@ -1,5 +1,5 @@
 
-$("#searchButton").on("click", function() {
+$(".btn-success").on("click", function() {
 var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json&query=" + search + "&api-key=82430d33598a4b5eb257ecac230cd536";	
 var search = "india";
 
