ea64bae6ef5c3592f8cff168aa4e6825c0ed4a49
Paul Richard <Paul Richard>
2017-05-13 13:57:21 -0700

    makes ajax calls when submit button is pressed

--- logic.js
+++ logic.js
@@ -1,13 +1,17 @@
 
 $(".btn-success").on("click", function() {
-var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json&query=" + search + "&api-key=82430d33598a4b5eb257ecac230cd536";	
-var search = "india";
-
+var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+url += '?' + $.param({
+  'api-key': "82430d33598a4b5eb257ecac230cd536",
+  'q': "india"
+});
 $.ajax({
-	url: url,
-	method: "GET",
-}).done(function(result){
-	console.log(result);
-})
+  url: url,
+  method: 'GET',
+}).done(function(result) {
+  console.log(result);
+}).fail(function(err) {
+  throw err;
+});
 });
 
