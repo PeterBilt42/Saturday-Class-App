a1432f9128118711120621a2bec44f5bc67c3d9e
Paul Richard <Paul Richard>
2017-05-13 13:37:43 -0700

    testing with India

--- logic.js
+++ logic.js
@@ -1,1 +1,13 @@
-var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+
+$("#searchButton").on("click", function() {
+var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json&query=" + search + "&api-key=82430d33598a4b5eb257ecac230cd536";	
+var search = "india";
+
+$.ajax({
+	url: url,
+	method: "GET",
+}).done(function(result){
+	console.log(result);
+})
+});
+
